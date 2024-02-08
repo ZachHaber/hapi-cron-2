e09db4a746140dada53752fac5d26d2e47985417
Zachary Soare <zachary.soare@abbvie.com>
2024-02-08 16:49:33 -0600

    chore(test): add tests for the new functions

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,13 +1,7 @@
-/*********************************************************************************
- 1. Dependencies
- *********************************************************************************/
 import { Server } from "@hapi/hapi";
 import { describe, expect, it, jest } from "@jest/globals";
 import HapiCron from "../lib/index.js";
-
-/*********************************************************************************
- 2. Exports
- *********************************************************************************/
+import { assert, baseJob, createServer, runServer } from "./_utils.js";
 
 describe("registration assertions", () => {
 	it("should register plugin without errors", async () => {
@@ -25,24 +19,7 @@ describe("registration assertions", () => {
 			await server.register({
 				plugin: HapiCron,
 				options: {
-					jobs: [
-						{
-							name: "testname",
-							crontab: "*/10 * * * * *",
-							timezone: "Europe/London",
-							request: {
-								url: "/test-url",
-							},
-						},
-						{
-							name: "testname",
-							crontab: "*/10 * * * * *",
-							timezone: "Europe/London",
-							request: {
-								url: "/test-url",
-							},
-						},
-					],
+					jobs: [baseJob, baseJob],
 				},
 			});
 		} catch (/** @type {any} */ err) {
@@ -247,27 +224,13 @@ describe("registration assertions", () => {
 
 describe("plugin functionality", () => {
 	it("should expose access to the registered jobs", async () => {
-		const server = new Server();
+		const { plugin } = await createServer();
 
-		await server.register({
-			plugin: HapiCron,
-			options: {
-				jobs: [
-					{
-						name: "testcron",
-						crontab: "*/10 * * * * *",
-						timezone: "Europe/London",
-						request: {
-							method: "GET",
-							url: "/test-url",
-						},
-					},
-				],
-			},
-		});
+		expect(plugin).toBeDefined();
+		expect(plugin.jobs.has(baseJob.name)).toBe(true);
 
-		expect(server.plugins.hapiCron).toBeDefined();
-		expect(server.plugins.hapiCron.jobs.has("testcron")).toBe(true);
+		expect(plugin.getJob).toBeDefined();
+		expect(plugin.getJob(baseJob.name)).toBeDefined();
 	});
 
 	it("should ensure the request and callback from the plugin options are triggered", (done) => {
@@ -279,16 +242,10 @@ describe("plugin functionality", () => {
 				options: {
 					jobs: [
 						{
-							name: "testcron",
-							crontab: "*/10 * * * * *",
-							timezone: "Europe/London",
-							request: {
-								method: "GET",
-								url: "/test-url",
-							},
+							...baseJob,
 							onComplete: (result) => {
 								expect(result.result).toBe("hello world");
-								done();
+								server.stop().then(() => done());
 							},
 						},
 					],
@@ -304,88 +261,121 @@ describe("plugin functionality", () => {
 				server.events.on("response", (request) => {
 					expect(request.method).toBe("get");
 					expect(request.path).toBe("/test-url");
-					// done();
 				});
 
-				// expect(onComplete).not.toHaveBeenCalled();
-
-				const job = server.plugins.hapiCron.jobs.get("testcron");
+				const job = server.plugins.hapiCron.jobs.get(baseJob.name);
 				expect(job).toBeDefined();
 				job?.cron?.fireOnTick();
 			});
-
-		// job?.addCallback
-		// await server.plugins.hapiCron.jobs
-		// 	.get("testcron")
-		// 	// @ts-ignore
-		// 	?._callbacks[0]();
-		// expect(onComplete).toHaveBeenCalledTimes(1);
-		// expect(onComplete).toHaveBeenCalledWith("hello world");
 	}, 500);
 
 	it("should not start the jobs until the server starts", async () => {
-		const server = new Server();
+		const { server, plugin } = await createServer();
 
-		await server.register({
-			plugin: HapiCron,
-			options: {
-				jobs: [
-					{
-						name: "testcron",
-						crontab: "*/10 * * * * *",
-						timezone: "Europe/London",
-						request: {
-							method: "GET",
-							url: "/test-url",
-						},
-					},
-				],
-			},
+		expect(plugin.jobs.get(baseJob.name)?.cron.running).toBe(false);
+
+		await runServer(server, () => {
+			expect(plugin.jobs.get(baseJob.name)?.cron.running).toBe(true);
 		});
-
-		expect(server.plugins.hapiCron.jobs.get("testcron")?.cron.running).toBe(
-			false,
-		);
-
-		await server.start();
-
-		expect(server.plugins.hapiCron.jobs.get("testcron")?.cron.running).toBe(
-			true,
-		);
-
-		await server.stop();
 	});
 
 	it("should stop cron jobs when the server stops", async () => {
-		const server = new Server();
+		const { server, plugin } = await createServer();
 
-		await server.register({
-			plugin: HapiCron,
-			options: {
-				jobs: [
-					{
-						name: "testcron",
-						crontab: "*/10 * * * * *",
-						timezone: "Europe/London",
-						request: {
-							method: "GET",
-							url: "/test-url",
-						},
-					},
-				],
-			},
+		await runServer(server, () => {
+			expect(plugin.jobs.get(baseJob.name)?.cron.running).toBe(true);
 		});
 
-		await server.start();
+		expect(plugin.jobs.get(baseJob.name)?.cron.running).toBe(false);
+	});
+});
 
-		expect(server.plugins.hapiCron.jobs.get("testcron")?.cron.running).toBe(
-			true,
-		);
+describe("toggleJob", () => {
+	it("should allow toggling a job", async () => {
+		const { server, plugin } = await createServer();
+		const job = plugin.getJob(baseJob.name);
+		assert(job);
+		await runServer(server, () => {
+			plugin.toggleJob(job.name);
 
-		await server.stop();
+			expect(job.cron.running).toBe(false);
 
-		expect(server.plugins.hapiCron.jobs.get("testcron")?.cron.running).toBe(
-			false,
-		);
+			plugin.toggleJob(job.name, false);
+			expect(job.cron.running).toBe(false);
+
+			plugin.toggleJob(job.name, true);
+			expect(job.cron.running).toBe(true);
+
+			plugin.toggleJob(job.name, true);
+			expect(job.cron.running).toBe(true);
+		});
+	});
+	it("should prevent toggling if the server isn't running", async () => {
+		const { plugin } = await createServer();
+		const job = plugin.getJob(baseJob.name);
+		assert(job);
+		plugin.toggleJob(baseJob.name);
+		expect(job.cron.running).toBe(false);
+	});
+});
+
+describe("addJob", () => {
+	it("should add a new job", async () => {
+		const { plugin } = await createServer();
+		const name = "newName";
+		plugin.addJob({ ...baseJob, name });
+		assert(plugin.getJob(name));
+		expect(plugin.getJob(name)?.cron.running).toBe(false);
+	});
+	it("should replace a job with the same name", async () => {
+		const { plugin } = await createServer();
+		const crontab = "5 0 * 8 *";
+		plugin.addJob({ ...baseJob, crontab });
+		const job = plugin.getJob(baseJob.name);
+		assert(job);
+		expect(job.crontab).toBe(crontab);
+		expect;
+	});
+	it("should stop a job with the same name before starting the new one", async () => {
+		const { server, plugin } = await createServer();
+		const crontab = "5 0 * 8 *";
+		const job = plugin.getJob(baseJob.name);
+		assert(job);
+		await runServer(server, () => {
+			assert(job.cron.running);
+
+			plugin.addJob({ ...baseJob, crontab });
+			expect(job.cron.running).toBe(false);
+			const newJob = plugin.getJob(baseJob.name);
+			assert(newJob);
+			expect(newJob.crontab).toBe(crontab);
+			assert(newJob.cron.running);
+		});
+	});
+});
+
+describe("removeJob", () => {
+	it("should remove a job", async () => {
+		const { server, plugin } = await createServer();
+		const job = plugin.getJob(baseJob.name);
+		assert(job);
+		assert(plugin.removeJob(job.name));
+		assert(!plugin.getJob(baseJob.name));
+	});
+
+	it("should stop a job before removing it", async () => {
+		const { server, plugin } = await createServer();
+		const job = plugin.getJob(baseJob.name);
+		assert(job);
+		await runServer(server, () => {
+			assert(job.cron.running);
+			assert(plugin.removeJob(job.name));
+			assert(!job.cron.running);
+		});
+	});
+
+	it("should return false if there is no job to remove", async () => {
+		const { server, plugin } = await createServer();
+		expect(plugin.removeJob("noJob")).toBe(false);
 	});
 });
